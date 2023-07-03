35e9d58d21045acf9b4379163c885d7a0b04b176
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-07-03 09:56:44 +0200

    initial commit

---
+++ src/napari_signal_selector/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.1"
+from ._sample_data import make_sample_data
+from ._widget import ExampleQWidget, example_magic_widget
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+    "write_single_image",
+    "write_multiple",
+    "make_sample_data",
+    "ExampleQWidget",
+    "example_magic_widget",
+)
