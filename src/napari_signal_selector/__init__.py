86100a233c5f3780ba7ceef41f255c449150f31c
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-08-04 09:15:27 +0200

    fix entry points for menu

--- src/napari_signal_selector/__init__.py
+++ src/napari_signal_selector/__init__.py
@@ -1,12 +1,8 @@
 __version__ = "0.0.1"
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
-from ._writer import write_multiple, write_single_image
+from .line import InteractiveFeaturesLineWidget
 
 __all__ = (
-    "write_single_image",
-    "write_multiple",
+    "InteractiveFeaturesLineWidget",
     "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
 )
