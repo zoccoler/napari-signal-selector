8e8c9bf716ce9fb9e09e61831571b1e469ee3bb8
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-10-17 13:22:41 +0200

    fix sample data shape

--- src/napari_signal_selector/_sample_data.py
+++ src/napari_signal_selector/_sample_data.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
-from pathlib import Path
-
-DATA_PATH = Path(__file__).parent / "data"
 
 def load_flashing_polygons_data():
     """Generates an image"""
     from skimage.io import imread
     from pandas import read_csv
     from numpy import newaxis
+    from pathlib import Path
+
+    DATA_PATH = Path(__file__).parent / "data"
    
     timelapse = imread(DATA_PATH / "synthetic_timelapse.tif")
-    timelapse = timelapse[:, newaxis]
     labels = imread(DATA_PATH / "synthetic_labels.tif")
     table = read_csv(DATA_PATH / "table_synthetic_data_with_annotations.csv")
     return [(timelapse, {'name': 'Synthetic Timelapse'}), 
