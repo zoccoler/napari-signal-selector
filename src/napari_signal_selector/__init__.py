12b00aeb94c5f251c76ff18af48b7831467546d5
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-10-15 17:12:29 +0200

    Add blinking polygons (sub-signals) sample data

--- src/napari_signal_selector/__init__.py
+++ src/napari_signal_selector/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "0.0.5"
-from ._sample_data import load_flashing_polygons_data
+from ._sample_data import load_flashing_polygons_data, load_blinking_polygons_data
 from .interactive import InteractiveFeaturesLineWidget
 
 __all__ = (
     "InteractiveFeaturesLineWidget",
     "load_flashing_polygons_data",
+    "load_blinking_polygons_data",
 )
--- src/napari_signal_selector/_sample_data.py
+++ src/napari_signal_selector/_sample_data.py
@@ -12,5 +12,21 @@ def load_flashing_polygons_data():
     timelapse = imread(DATA_PATH / "synthetic_timelapse.tif")
     labels = imread(DATA_PATH / "synthetic_labels.tif").astype('uint32')
     table = read_csv(DATA_PATH / "table_synthetic_data_with_annotations.csv")
-    return [(timelapse, {'name': 'Synthetic Timelapse'}), 
-            (labels, {'name': 'Synthetic Labels', 'features': table, 'opacity': 0.4}, 'labels')]
+    return [(timelapse, {'name': 'Flashing Polygons Synthetic Timelapse'}), 
+            (labels, {'name': 'Flashing Polygons Synthetic Labels', 'features': table, 'opacity': 0.4}, 'labels')]
+
+
+def load_blinking_polygons_data():
+    """Generates an image"""
+    from skimage.io import imread
+    from pandas import read_csv
+    from numpy import newaxis
+    from pathlib import Path
+
+    DATA_PATH = Path(__file__).parent / "data"
+   
+    timelapse = imread(DATA_PATH / "synthetic_timelapse_sub_signals.tif")
+    labels = imread(DATA_PATH / "synthetic_labels_sub_signals.tif").astype('uint32')
+    table = read_csv(DATA_PATH / "table_synthetic_data_with_annotations_sub_signals.csv")
+    return [(timelapse, {'name': 'Blinking Polygons Synthetic Timelapse'}), 
+            (labels, {'name': 'Blinking Polygons Synthetic Labels Timelapse', 'features': table, 'opacity': 0.4}, 'labels')]
